1ed0b12f35e116c6de313fbfd12d5e982f0aa6cf
Nialevolstel <nialevolstel@gmail.com>
2018-11-29 04:18:03 -0300

    First Commit

---
+++ Assets/Scripts/ChenMouth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChenMouth : MonoBehaviour {
+
+    Animator m_animator;
+
+    private string m_animation_parameter = "CurrentState";
+    private int m_animation_idle = 0;
+    private int m_animation_speak = 1;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    public void invokeSpeak()
+    {
+        if (m_animator != null)
+        {
+            m_animator.SetInteger(m_animation_parameter, m_animation_speak);
+        }
+    }
+
+
+    public void invokeIdle()
+    {
+        if (m_animator != null)
+        {
+            m_animator.SetInteger(m_animation_parameter, m_animation_idle);
+        }
+    }
+}
---
+++ Assets/Scripts/Recognition.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using UnityEngine;
+
+// Namespace that provides the Microsoft API for Speech Recognition
+// Requires Unity 5.4.0b2 and above to work on Windows
+using UnityEngine.Windows.Speech;
+
+public class Recognition : MonoBehaviour {
+
+    [Serializable]
+    public class KeyActionPair
+    {
+        public string m_keyword;
+        public UnityEngine.Events.UnityEvent m_action;
+    }
+
+    // Confidence Level for the SpeechRecognition. Phrases under the specified minimum level will be ignored.
+    [SerializeField]
+    ConfidenceLevel m_ConfidenceLevel;
+
+    // List of keywords that should be recognized by the Keyword Recognizer to invoke their respective events.
+    [SerializeField]
+    private KeyActionPair[] m_KeyActions;
+
+    // Object that listens to speech input and attempts to match uttered phrases to a list of registered keywords.
+    private KeywordRecognizer m_Recognizer;
+
+    // Dictionary that optimize the relation lookup between a keyword and it's action
+    private IDictionary<string, UnityEngine.Events.UnityEvent> m_KeyDictionary;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("MUSE! MUSICAL! STARTO!");
+
+        initializeDictionary();
+        initializeRecognizer();
+
+    }
+
+    private void initializeDictionary()
+    {
+        m_KeyDictionary = new Dictionary<string, UnityEngine.Events.UnityEvent>();
+        foreach (KeyActionPair ka in m_KeyActions)
+        {
+            m_KeyDictionary.Add(new KeyValuePair<string, UnityEngine.Events.UnityEvent>(ka.m_keyword, ka.m_action));
+        }
+    }
+
+    private void initializeRecognizer()
+    {
+        // InitializeDictionary should be called first
+        string[] keywords = m_KeyDictionary.Keys.ToArray();
+        m_Recognizer = new KeywordRecognizer(keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+	
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(builder.ToString());
+        m_KeyDictionary[args.text].Invoke();
+
+    }
+
+}
